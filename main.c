4a372c8450c3e6e81111e2a5bf7bf5c3579dff37
Justchidi26 <chidiaguwa26@gmail.com>
2025-08-27 16:25:39 +0100

    Calulator fix

--- main.c
+++ main.c
@@ -1,13 +1,64 @@
-#include "include/input_utils.h"
 #include <stdio.h>
 
-int main() {
-    int age = get_int("Enter your age: ");
-    string first_name = get_string("Enter your name: ");
-    printf("Hello, %s! You are %d years old.\n", first_name, age);
-    string last_name = get_string("Enter your last name: ");
-    printf("Your full name is: %s %s\n", first_name, last_name);
+int main(void) {
+    double a, b, answer;
+    char c;
+    int validInput = 0;
+
+    
+    while (!validInput) {
+        printf("Enter a number: ");
+        if (scanf("%lf", &a) != 1) {
+            printf("Error: Invalid input. Please enter a number.\n");
+            while (getchar() != '\n'); 
+        } else {
+            validInput = 1;
+        }
+    }
+
+    validInput = 0;
+    while (!validInput) {
+        printf("Please enter an operator (+, -, *, /): ");
+        scanf(" %c", &c);
+        if (c == '+' || c == '-' || c == '*' || c == '/') {
+            validInput = 1;
+        } else {
+            printf("Invalid operator. Try again.\n");
+        }
+    }
+
+    
+    validInput = 0;
+    while (!validInput) {
+        printf("Enter another number: ");
+        if (scanf("%lf", &b) != 1) {
+            printf("Error: Invalid input. Please enter a number.\n");
+            while (getchar() != '\n'); 
+        } else {
+            validInput = 1;
+        }
+    }
+
+    
+    int success = 1;
+    if (c == '+') {
+        answer = a + b;
+    } else if (c == '-') {
+        answer = a - b;
+    } else if (c == '*') {
+        answer = a * b;
+    } else if (c == '/') {
+        if (b == 0) {
+            printf("Invalid. Division by 0 is not allowed.\n");
+            success = 0;
+        } else {
+            answer = a / b;
+        }
+    }
+
+    if (success) {
+        printf("Answer is: %.2lf\n", answer);
+    }
+
     return 0;
 }
-
-// cp -r c-boilerplate new-project-name
